8e8a960c256a83c0df479a3e65ddba8ddba57f83
thomas emile bourgeat <bthom@wolff.csail.mit.edu>
2017-10-12 11:54:04 -0400

    Coq printer spit out something, untested

--- src/CoqPrint.hs
+++ src/CoqPrint.hs
@@ -3,51 +3,56 @@ import Debug.Trace (trace)
 import Control.Monad
 import Data.List
 
-class CoqPrint a where
-  coqPrint :: a -> String
-
-instance CoqPrint Exp where
-  coqPrint (If cond ifT ifF) =
+coqPrint (If cond ifT ifF) =
     (++ "\n") . join . (intersperse " ")  $ ["if", coqPrint cond, "then", coqPrint ifT, "else" , coqPrint ifF]
-  coqPrint (Arith TokenMPLUS l) =
+coqPrint (Arith TokenMPLUS l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " + " $ fmap coqPrint l
-  coqPrint (Arith TokenMTIMES l) =
+coqPrint (Arith TokenMTIMES l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " * " $ fmap coqPrint l
-  coqPrint (Arith TokenMMINUS [a]) = "(-" ++ coqPrint a ++")"
-  coqPrint (Arith TokenMMINUS l) =
+coqPrint (Arith TokenMMINUS [a]) = "(-" ++ coqPrint a ++")"
+coqPrint (Arith TokenMMINUS l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " - " $ fmap coqPrint l
-  coqPrint (Arith TokenMOR l) =
+coqPrint (Arith TokenMOR l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " .|. " $ fmap coqPrint l
-  coqPrint (Arith TokenMAND l) =
+coqPrint (Arith TokenMAND l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " .&. " $ fmap coqPrint l
-  coqPrint (Arith TokenMBAND l) =
+coqPrint (Arith TokenMBAND l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " and " $ fmap coqPrint l
-  coqPrint (Arith TokenMBOR l) =
+coqPrint (Arith TokenMBOR l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " or " $ fmap coqPrint l
-  coqPrint (Arith TokenMTYPEOF l) =
+coqPrint (Arith TokenMTYPEOF l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " : " $ fmap coqPrint l
-  coqPrint (Arith TokenMEQUAL l) =
+coqPrint (Arith TokenMEQUAL l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " == " $ fmap coqPrint l
-  coqPrint (Arith TokenMDIFF l) =
+coqPrint (Arith TokenMDIFF l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " /= " $ fmap coqPrint l
-  coqPrint (Arith TokenMLT l) =
+coqPrint (Arith TokenMLT l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " < " $ fmap coqPrint l
-  coqPrint (Arith TokenMGT l) =
+coqPrint (Arith TokenMGT l) =
    (\l-> "(" ++ l ++ ")") . join . intersperse " > " $ fmap coqPrint l
-  coqPrint (App a b) =
+coqPrint (App a b) =
     join . intersperse " " $ ["(", coqPrint a, ")", "(", coqPrint b, ")"]
-  coqPrint (Num a) = show a
-  coqPrint (Iden s) = s
-  coqPrint (Do ((Bind name b):q)) = join . intersperse " " $ [coqPrint b, ">>=\n", "(fun", name,"=>", coqPrint (Do q),")"]
-  coqPrint (Do ([t])) = join . intersperse " " $ ["(", coqPrint t, ")"]
-  coqPrint (Do (t:q)) = join . intersperse " " $ ["(", coqPrint t, ">>\n",  coqPrint (Do q),")"]
-  coqPrint (Do []) = ""
-  coqPrint (Let s l body) = join . intersperse " " $ ["let", s, "=", nestedIf l, "in\n", coqPrint body]
-  coqPrint x = trace (show x) undefined
+coqPrint (Num a) = show a
+coqPrint (Iden s) = s
+coqPrint (Do ((Bind name b):q)) = join . intersperse " " $ [coqPrint b, ">>=\n", "(fun", name,"=>", coqPrint (Do q),")"]
+coqPrint (Do ([t])) = join . intersperse " " $ ["(", coqPrint t, ")"]
+coqPrint (Do (t:q)) = join . intersperse " " $ ["(", coqPrint t, ">>\n",  coqPrint (Do q),")"]
+coqPrint (Do []) = ""
+coqPrint (Let s l body) = join . intersperse " " $ ["let", s, "=", nestedIf l, "in\n", coqPrint body]
+coqPrint x = trace (show x) undefined
+
+
+printMatcher (App a b) = join . intersperse " " $ [printMatcher a, printMatcher b]
+printMatcher (Iden s) = s
+
+
+executeCoq ((ExecuteCase a b):q) = join . intersperse " " $ ["|", printMatcher a, "=>", coqPrint b, "\n", executeCoq q]
+executeCoq [] = ""
 
 nestedIf [(Iden "otherwise",t)] = coqPrint t
 nestedIf ((c,t):q) = join .intersperse " " $ ["(","if", coqPrint c, "then\n", coqPrint t, "else\n", nestedIf q, ")"]
 
-main = undefined
 
-  
+transliterateCoq l ="Definition execute (instr: Instruction) : Riscv :=\nmatch instr with" ++ executeCoq l ++ "\nend."
+
+main = undefined
