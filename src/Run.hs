bb67946727c318a3973a4555fadb289d891bca3d
acw1251 <acwright@mit.edu>
2017-11-29 03:28:30 -0500

    Added tohost support to Run.hs for riscv-tests

--- src/Run.hs
+++ src/Run.hs
@@ -44,32 +44,42 @@ checkInterrupt = do
     else return False
   else return False
 
-helper :: IOState Minimal64 Int64
-helper = do
-  pc <- getPC
-  -- trace (show pc) $return ()
-  inst <- loadWord pc
-  if inst == 0x6f -- Stop on infinite loop instruction.
+helper :: Maybe Int64 -> IOState Minimal64 Int64
+helper maybeToHostAddress = do
+  toHostValue <- case maybeToHostAddress of
+    Nothing -> return 0 -- default value
+    Just toHostAddress -> loadWord toHostAddress
+  if toHostValue /= 0
     then do
-        cycles <- getCSRField Field.MCycle
-        trace ("Cycles: " ++ show cycles) (return ())
-        instret <- getCSRField Field.MInstRet
-        trace ("Insts: " ++ show instret) (return ())
-        getRegister 10
+      -- quit running
+      if toHostValue == 1
+        then trace "PASSED" (return 0)
+        else trace ("FAILED " ++ (show $ quot toHostValue 2)) (return 1)
     else do
-    setPC (pc + 4)
-    size <- getXLEN
-    execute (decode size $ fromIntegral inst)
-    interrupt <- liftIO checkInterrupt
-    if interrupt then do
-      -- Signal interrupt by setting MEIP high.
-      setCSRField Field.MEIP 1
-    else return ()
-    step
-    helper
+      pc <- getPC
+      -- trace (show pc) $return ()
+      inst <- loadWord pc
+      if inst == 0x6f -- Stop on infinite loop instruction.
+        then do
+            cycles <- getCSRField Field.MCycle
+            trace ("Cycles: " ++ show cycles) (return ())
+            instret <- getCSRField Field.MInstRet
+            trace ("Insts: " ++ show instret) (return ())
+            getRegister 10
+        else do
+        setPC (pc + 4)
+        size <- getXLEN
+        execute (decode size $ fromIntegral inst)
+        interrupt <- liftIO checkInterrupt
+        if interrupt then do
+          -- Signal interrupt by setting MEIP high.
+          setCSRField Field.MEIP 1
+        else return ()
+        step
+        helper maybeToHostAddress
 
-runProgram :: Minimal64 -> IO (Int64, Minimal64)
-runProgram = runStateT helper
+runProgram :: Maybe Int64 -> Minimal64 -> IO (Int64, Minimal64)
+runProgram maybeToHostAddress = runStateT (helper maybeToHostAddress)
 
 runFile :: String -> IO Int64
 runFile f = do
@@ -77,14 +87,16 @@ runFile f = do
   m <- readHexFile h []
   let c = Minimal64 { registers = (take 31 $ repeat 0), csrs = emptyFile, pc = 0x200, nextPC = 0,
                       mem = S.fromList $ zip [0..] (m ++ (take (65520 - length m) $ repeat (0::Word8))) } in
-    fmap fst $ runProgram c
+    fmap fst $ runProgram Nothing c
 
 runElf :: String -> IO Int64
 runElf f = do
   m <- readElf f
+  -- converts Word32 to Int32
+  maybeToHostAddress <- fmap (fmap (fromInteger . toInteger)) $ readElfSymbol "tohost" f
   let c = Minimal64 { registers = (take 31 $ repeat 0), csrs = emptyFile, pc = 0x80000000, nextPC = 0,
                       mem = S.fromList m } in
-    fmap fst $ runProgram c
+    fmap fst $ runProgram maybeToHostAddress c
 
 main :: IO ()
 main = do
