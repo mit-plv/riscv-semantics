c3976cd10770d910fd203f29d69d1e3558873684
ijc <ijc@mit.edu>
2018-02-27 21:45:53 -0500

    Rewrite BufferMMIO and Test, update Travis script to run our tests.
    The cabal script now generates a second executable, riscv-semantics-tests,
    which runs our tests and returns 0 if they all pass (based on both the return
    value and standard output). The Travis script now runs this executable.

--- src/BufferMMIO.hs
+++ src/BufferMMIO.hs
@@ -1,85 +1,69 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, MultiWayIf #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, MultiWayIf, UndecidableInstances #-}
 module BufferMMIO where
-import Prelude hiding (getChar, putChar)
+import Data.Bits
+import Data.Int
+import Data.Char
+import Control.Monad.Identity
+import Control.Monad.State
+import Control.Monad.Writer
+import qualified Data.Map as S
+
 import Program
 import Utility
-import Data.Int
-import Data.Word
-import Data.Bits
-import Data.Char
-import Control.Applicative
-import Control.Monad
 
-newtype MState s a = MState { runState :: s -> Maybe (a, s) }
+-- Simple State monad to simulate IO. The first string represents input, the
+-- second represents output.
+type BufferIO = State (String, String)
 
-instance Functor (MState s) where
-  fmap f a = MState $ \state -> fmap (\(b,s) -> (f b, s)) (runState a state)
+runBufferIO :: BufferIO a -> String -> (a, String)
+runBufferIO m input = (result, output)
+  where (result, (_, output)) = runState m (input, "")
 
-instance Applicative (MState s) where
-  pure x = MState $ \state -> Just (x, state)
-  (<*>) f a = MState $ \state -> do
-    (g, s1) <- runState f state
-    (b, s2) <- runState a s1
-    return (g b, s2)
+type BufferState s = StateT s BufferIO
 
-instance Monad (MState s) where
-  (>>=) a f = MState $ \state -> runState a state >>= (\(b, s) -> runState (f b) s)
+type LoadFunc s = BufferState s Int32
+type StoreFunc s = Int32 -> BufferState s ()
 
-instance Alternative (MState s) where
-  empty = MState $ \state -> Nothing
-  (<|>) a b = MState $ \state -> runState a state <|> runState b state
+instance (Show (LoadFunc s)) where
+  show _ = "<io/loadfunc>"
+instance (Show (StoreFunc s)) where
+  show _ = "<io/storefunc>"
 
-instance MonadPlus (MState s)
+bufferGetChar :: BufferIO Int32
+bufferGetChar = state $ \((input, output)) ->
+                          if null input then (-1, (input, output))
+                          else (fromIntegral $ ord $ head input, (tail input, output))
+bufferPutChar :: Int32 -> BufferIO ()
+bufferPutChar c = state $ \((input, output)) -> ((), (input, output ++ [chr $ fromIntegral c]))
 
-data MMIO32 = MMIO32 { registers :: [Int32], pc :: Int32, nextPC :: Int32, mem :: [Word8], mmio :: [(LoadFunc, StoreFunc)], input :: String, output :: String }
-                deriving (Show)
+rvGetChar :: LoadFunc s
+rvGetChar = lift bufferGetChar
+rvPutChar :: StoreFunc s
+rvPutChar val = lift (bufferPutChar val)
 
-type LoadFunc = MMIO32 -> (Int32, MMIO32)
-type StoreFunc = MMIO32 -> Int32 -> MMIO32
+-- Addresses for mtime/mtimecmp chosen for Spike compatibility.
+mmioTable :: S.Map MachineInt (LoadFunc s, StoreFunc s)
+mmioTable = S.fromList [(0xfff4, (rvGetChar, rvPutChar))]
 
-instance (Show LoadFunc) where
-  show x = "<loadfunc>"
-instance (Show StoreFunc) where
-  show x = "<storefunc>"
-
-helpStore mem addr bytes = f ++ bytes ++ drop (length bytes) s
-  where (f,s) = splitAt addr mem
-
-ord32 :: Char -> Int32
-ord32 = fromIntegral . ord
-
-chr32 :: Int32 -> Char
-chr32 = chr . fromIntegral
-
-tryHead "" = -1
-tryHead (x:xs) = ord32 x
-
-tryTail "" = ""
-tryTail (x:xs) = xs
-
-getChar :: MMIO32 -> (Int32, MMIO32)
-getChar comp = (tryHead $ input comp, comp { input = tryTail (input comp) })
-putChar :: MMIO32 -> Int32 -> MMIO32
-putChar comp val = comp { output = output comp ++ [chr32 val] }
-
-baseMMIO = [(getChar, putChar)]
-
-mmioStart :: (Num a) => MMIO32 -> a
-mmioStart = (+1) . fromIntegral . length . mem
-
-instance RiscvProgram (MState MMIO32) Int32 Word32 where
-  getRegister reg = MState $ \comp -> Just (if reg == 0 then 0 else (registers comp) !! (fromIntegral reg-1), comp)
-  setRegister reg val = MState $ \comp -> Just ((), if reg == 0 then comp else comp { registers = setIndex (fromIntegral reg-1) (fromIntegral val) (registers comp) })
-  loadByte addr = MState $ \comp -> Just (fromIntegral $ (mem comp) !! (fromIntegral addr), comp)
-  loadHalf addr = MState $ \comp -> Just (combineBytes $ take 2 $ drop (fromIntegral addr) (mem comp), comp)
-  loadWord addr = MState $ \comp -> if
-    | addr > (mmioStart comp) -> Just $ (fst ((mmio comp) !! ((fromIntegral $ addr - mmioStart comp) `div` 4))) comp
-    | otherwise -> Just (combineBytes $ take 4 $ drop (fromIntegral addr) (mem comp), comp)
-  storeByte addr val = MState $ \comp -> Just ((), comp { mem = setIndex (fromIntegral addr) (fromIntegral val) (mem comp) })
-  storeHalf addr val = MState $ \comp -> Just ((), comp { mem = helpStore (mem comp) (fromIntegral addr) (splitHalf val) })
-  storeWord addr val = MState $ \comp -> if
-    | addr > (mmioStart comp) -> Just ((), (snd ((mmio comp) !! ((fromIntegral $ addr - mmioStart comp) `div` 4))) comp (fromIntegral val))
-    | otherwise -> Just ((), comp { mem = helpStore (mem comp) (fromIntegral addr) (splitWord val) })
-  getPC = MState $ \comp -> Just (pc comp, comp)
-  setPC val = MState $ \comp -> Just ((), comp { nextPC = fromIntegral val })
-  step = MState $ \comp -> Just ((), comp { pc = nextPC comp })
+instance (RiscvProgram (State s) t u, Convertible t u, Bounded t, Bounded u, Bits t, Bits u, MachineWidth t) => RiscvProgram (BufferState s) t u where
+  getRegister r = liftState (getRegister r)
+  setRegister r v = liftState (setRegister r v)
+  loadByte a = liftState (loadByte a)
+  loadHalf a = liftState (loadHalf a)
+  loadWord addr =
+    case S.lookup (fromIntegral addr) mmioTable of
+      Just (getFunc, _) -> getFunc
+      Nothing -> liftState (loadWord addr)
+  loadDouble a = liftState (loadDouble a)
+  storeByte a v = liftState (storeByte a v)
+  storeHalf a v = liftState (storeHalf a v)
+  storeWord addr val =
+    case S.lookup (fromIntegral addr) mmioTable of
+      Just (_, setFunc) -> setFunc (fromIntegral val)
+      Nothing -> liftState (storeWord addr val)
+  storeDouble a v = liftState (storeDouble a v)
+  getCSRField f = liftState (getCSRField f)
+  setCSRField f v = liftState (setCSRField f v)
+  getPC = liftState getPC
+  setPC v = liftState (setPC v)
+  step = liftState step
