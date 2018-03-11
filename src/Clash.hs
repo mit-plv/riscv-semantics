8ea0393476e2d90f1a14d8837b402990d9b004bb
Thomas Bourgeat <bthom@mit.edu>
2018-03-11 18:40:40 -0400

    byteenable proper ordering

--- src/Clash.hs
+++ src/Clash.hs
@@ -28,16 +28,48 @@ data MMIOClash = MMIOClash { registers :: Vec 31 Int32, pc :: Int32, nextPC :: I
 type MState = State MMIOClash
 type MMState = MaybeT MState
 
+extract :: Integral a => (Bool,Bool,Bool,Bool) -> Int32 -> a
+extract byteen load | -- quot get rid of the bits on the right fromIntegral, those on the left.
+	byteen == (_,_,_,True) = fromIntegral load
+	byteen == (_,_,True,_) = fromIntegral (load `quot` 256)
+	byteen == (_,True,_,_) = fromIntegral (load `quot` 65536)
+	byteen == (True,_,_,_) = fromIntegral (load `quot` 16777216)
+
+ 
 instance RiscvProgram MState Int32 where
   getRegister reg = state $ \comp -> (if reg == 0 then 0 else (registers comp) !! (fromIntegral reg-1), comp)
   setRegister reg val = state $ \comp ->((), if reg == 0 then comp else comp { registers = replace (fromIntegral reg-1) (fromIntegral val) (registers comp) })
 -- Fake load and stores
-  loadByte a = state $ \comp -> (load comp, comp{loadAddress = Just $ fromIntegral (a, byteen)})
-  loadHalf a =state $ \comp -> (load comp, comp{loadAddress = Just $ fromIntegral (a, byteen)})
-  loadWord a = state $ \comp -> (load comp, comp{loadAddress = Just $ fromIntegral (a,(True,True,True,True))})
+  loadByte ina = state $ \comp -> let a = (ina `quot` 4) * 4 
+                                      offset = mod ina 4 
+                                      byteen | offset == 3 = (True,False,False,False)
+                                             | offset == 2 = (False,True,False,False)
+                                             | offset == 1 = (False,False,True,False)
+                                             | offset == 0 = (False,False,False,True) in
+                         (extract byteen (load comp), comp{loadAddress = Just $ (fromIntegral a, byteen)})
+  loadHalf ina =state $ \comp -> let a = (ina `quot` 4) * 4 -- zeroed the bottom two bits probably ot the smartest way Clash-wise
+                                     offset = mod ina 4 
+                                     byteen | offset == 2 = (True,True,False,False)
+                                            | offset == 0 = (False,False,True,True)
+                                            | otherwise = (False,False,False,False) -- Should not happen
+                         in
+                         (extract byteen (load comp), comp{loadAddress = Just $ (fromIntegral a, byteen)})
+  loadWord a = state $ \comp -> (load comp, comp{loadAddress = Just $ (fromIntegral a,(True,True,True,True))})
   loadDouble a = state $ \comp -> (0, comp)
-  storeByte a v = state $ \comp -> ((), comp{store=Just (fromIntegral a, fromIntegral v)})
-  storeHalf a v = state $ \comp -> ((), comp{store=Just (fromIntegral a, fromIntegral v)})
+  storeByte ina v = state $ \comp -> let a = (ina `quot` 4) * 4 -- zeroed the bottom two bits probably ot the smartest way Clash-wise
+                                         offset = mod ina 4 
+                                         byteen | offset == 3 = (True,False,False,False)
+                                                | offset == 2 = (False,True,False,False)
+                                                | offset == 1 = (False,False,True,False)
+                                                | offset == 0 = (False,False,False,True)
+                         in ((), comp{store=Just (fromIntegral a, fromIntegral v, byteen)})
+  storeHalf ina v = state $ \comp ->let a = (ina `quot` 4) * 4 -- zeroed the bottom two bits probably ot the smartest way Clash-wise
+                                        offset = mod ina 4 
+                                        byteen | offset == 2 = (True,True,False,False)
+                                               | offset == 0 = (False,False,True,True)
+                                               | otherwise = (False,False,False,False) -- Should not happen
+                         in
+                         ((), comp{store=Just (fromIntegral a, fromIntegral v, byteen)})
   storeWord  a v = state $ \comp -> ((), comp{store=Just (fromIntegral a, fromIntegral v,(True,True,True,True))})
   storeDouble  a v = state $ \comp -> ((), comp)
 -- fake CSR
