69d873708765187acb9b2f6d3c133ef7541191e0
Thomas Bourgeat <bthom@mit.edu>
2018-03-10 22:55:17 -0500

    Add unsupported instruction properly

--- src/ExecuteClash.hs
+++ src/ExecuteClash.hs
@@ -5,6 +5,7 @@ import qualified CSRField as Field
 import ExecuteI as I
 import ExecuteM as M
 import ExecuteCSR as CSR
+import ExecuteUnsupported as Unsupported
 import Control.Monad
 import Control.Monad.Trans.Maybe
 import Prelude 
@@ -14,7 +15,7 @@ execute InvalidInstruction = do
   cycles <- getCSRField Field.MCycle
   setCSRField Field.MCycle (cycles + 1)
 execute inst = do
-  (msum (map (\f -> f inst) [I.execute, CSR.execute]))
+  (msum (map (\f -> f inst) [I.execute, CSR.execute, Unsupported.execute]))
   cycles <- getCSRField Field.MCycle
   setCSRField Field.MCycle (cycles + 1)
   instret <- getCSRField Field.MInstRet
