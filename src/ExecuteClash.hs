832ced101d72bf3250c9f64e43a5213cea6f10db
Thomas Bourgeat <bthom@mit.edu>
2018-03-10 22:23:17 -0500

    Fix clash bug, though careful between the two maybe monads, I need to think about what happen if we run a Multiply instruction on a spec without multiply

--- src/ExecuteClash.hs
+++ src/ExecuteClash.hs
@@ -8,13 +8,13 @@ import ExecuteCSR as CSR
 import Control.Monad
 import Control.Monad.Trans.Maybe
 import Prelude 
-execute :: (RiscvProgram p t ) => Instruction -> p ()
+execute :: (RiscvProgram p t, MonadPlus p ) => Instruction -> p ()
 execute InvalidInstruction = do
   raiseException 0 2
   cycles <- getCSRField Field.MCycle
   setCSRField Field.MCycle (cycles + 1)
 execute inst = do
-  _ <- runMaybeT (msum (map (\f -> f inst) [I.execute, CSR.execute]))
+  (msum (map (\f -> f inst) [I.execute, CSR.execute]))
   cycles <- getCSRField Field.MCycle
   setCSRField Field.MCycle (cycles + 1)
   instret <- getCSRField Field.MInstRet
