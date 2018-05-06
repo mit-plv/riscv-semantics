08f24ee1b987331abc190a721db99123c0dba921
Thomas Bourgeat <bthom@mit.edu>
2018-05-06 09:45:01 -0400

    TLB experiment

--- src/TLBExperiment.hs
+++ src/TLBExperiment.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, MultiWayIf, UndecidableInstances #-}
-module  where
+module TLBExperiment where
+
 import Data.Bits
 import Data.Int
 import Data.Char
 import BufferMMIO
 import Control.Monad
+import Control.Monad.Trans
 import Control.Monad.Amb
 
 import Program
@@ -12,33 +14,27 @@ import Utility
 
 -- Simple State monad to simulate IO. The first string represents input, the
 -- second represents output.
-type BufferIO = State (String, String)
 
-runBufferIO :: BufferIO a -> String -> (a, String)
-runBufferIO m input = (result, output)
-  where (result, (_, output)) = runState m (input, "")
 
-type BufferState s = StateT s BufferIO
-
-instance (RiscvProgram (BufferState s) t, MachineWidth t) => RiscvProgram ( s) t where
-  getRegister r = liftState (getRegister r)
-  setRegister r v = liftState (setRegister r v)
-  loadByte a = liftState (loadByte a)
-  loadHalf a = liftState (loadHalf a)
-  loadWord addr = liftState (loadWord addr)
-  loadDouble a = liftState (loadDouble a)
-  storeByte a v = liftState (storeByte a v)
-  storeHalf a v = liftState (storeHalf a v)
-  storeWord addr val = liftState (storeWord addr val)
-  storeDouble a v = liftState (storeDouble a v)
-  getCSRField f = liftState (getCSRField f)
-  setCSRField f v = liftState (setCSRField f v)
-  getPC = liftState getPC
-  setPC v = liftState (setPC v)
-  getPrivMode = liftState getPrivMode
-  setPrivMode v = liftState (setPrivMode v)
-  commit = liftState commit
-  endCycle = liftState endCycle
-  inTLB a = liftState (inTLB a) -- noTLB
-  addTLB a b c = liftState (addTLB a b c)
+instance (RiscvProgram (s) t, MachineWidth t) => RiscvProgram (AmbT r s) t where
+  getRegister r = lift(getRegister r)
+  setRegister r v = lift(setRegister r v)
+  loadByte a = lift(loadByte a)
+  loadHalf a = lift(loadHalf a)
+  loadWord addr = lift(loadWord addr)
+  loadDouble a = lift(loadDouble a)
+  storeByte a v = lift(storeByte a v)
+  storeHalf a v = lift(storeHalf a v)
+  storeWord addr val = lift(storeWord addr val)
+  storeDouble a v = lift(storeDouble a v)
+  getCSRField f = lift(getCSRField f)
+  setCSRField f v = lift(setCSRField f v)
+  getPC = lift getPC
+  setPC v = lift (setPC v)
+  getPrivMode = lift getPrivMode
+  setPrivMode v = lift (setPrivMode v)
+  commit = lift commit
+  endCycle = lift endCycle
+  inTLB a = lift (inTLB a) -- noTLB
+  addTLB a b c = lift (addTLB a b c)
   
