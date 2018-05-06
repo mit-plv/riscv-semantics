0ad95b13fff6e1b30490f4316ab13a2538e54204
Thomas Bourgeat <bthom@mit.edu>
2018-05-06 15:25:58 -0400

    Parameters inversed

--- src/TLBExperiment.hs
+++ src/TLBExperiment.hs
@@ -20,37 +20,40 @@ import Utility
 
 -- Simple State monad to simulate IO. The first string represents input, the
 -- second represents output.
-type Tlb = (State [Map.Map MachineInt (MachineInt,Int)])
-type TlbState s = StateT s Tlb 
+type Tlb = ([Map.Map MachineInt (MachineInt,Int)])
+type TlbState s = StateT Tlb s 
 
-getTlbs :: Tlb ([Map.Map MachineInt (MachineInt,Int)])
-getTlbs = state $ \l -> (l,l)
-putTlbs :: [Map.Map MachineInt (MachineInt,Int)] -> Tlb ()
-putTlbs new = state $ \l -> ((),new)
+--getTlbs :: Tlb ([Map.Map MachineInt (MachineInt,Int)])
+--getTlbs = state $ \l -> (l,l)
+--putTlbs :: [Map.Map MachineInt (MachineInt,Int)] -> Tlb ()
+--putTlbs new = state $ \l -> ((),new)
+--runTlb :: Tlb a -> [Map.Map MachineInt (MachineInt,Int)] -> a
+--runTlb m input = fst $ runState m input
+--
 
 
 
-instance (RiscvProgram (State s) t, MachineWidth t) => RiscvProgram (TlbState   s  ) t where
-  getRegister r = liftState (getRegister r)
-  setRegister r v = liftState (setRegister r v)
-  loadByte a = liftState (loadByte a)
-  loadHalf a = liftState (loadHalf a)
-  loadWord addr = liftState (loadWord addr)
-  loadDouble a = liftState (loadDouble a)
-  storeByte a v = liftState (storeByte a v)
-  storeHalf a v = liftState (storeHalf a v)
-  storeWord addr val = liftState (storeWord addr val)
-  storeDouble a v = liftState (storeDouble a v)
-  getCSRField f = liftState (getCSRField f)
-  setCSRField f v = liftState (setCSRField f v)
-  getPC = liftState getPC
-  setPC v = liftState (setPC v)
-  getPrivMode = liftState getPrivMode
-  setPrivMode v = liftState (setPrivMode v)
-  commit = liftState commit
-  endCycle = liftState endCycle
+instance (RiscvProgram (s) t, MachineWidth t) => RiscvProgram (TlbState   s  ) t where
+  getRegister r = lift (getRegister r)
+  setRegister r v = lift(setRegister r v)
+  loadByte a = lift (loadByte a)
+  loadHalf a = lift (loadHalf a)
+  loadWord addr = lift (loadWord addr)
+  loadDouble a = lift (loadDouble a)
+  storeByte a v = lift (storeByte a v)
+  storeHalf a v = lift (storeHalf a v)
+  storeWord addr val = lift (storeWord addr val)
+  storeDouble a v = lift (storeDouble a v)
+  getCSRField f = lift (getCSRField f)
+  setCSRField f v = lift (setCSRField f v)
+  getPC = lift getPC
+  setPC v = lift (setPC v)
+  getPrivMode = lift getPrivMode
+  setPrivMode v = lift (setPrivMode v)
+  commit = lift commit
+  endCycle = lift endCycle
   inTLB a = do
-           tlbLevels <- lift getTlbs
+           tlbLevels <- get
            mode <- fmap getMode (getCSRField Field.MODE)
            let founds = imap (\idx tlbLevel->Map.lookup (getVPN mode a (idx+1)) tlbLevel) tlbLevels 
            return . listToMaybe . catMaybes $ fmap (\found -> case found of 
@@ -59,8 +62,8 @@ instance (RiscvProgram (State s) t, MachineWidth t) => RiscvProgram (TlbState   
                            
   addTLB addr pte level = do
       mode <- fmap getMode (getCSRField Field.MODE)
-      tlbLevels <- lift getTlbs
-      lift . putTlbs . imap (\idx tlbLevel -> 
+      tlbLevels <- get
+      put . imap (\idx tlbLevel -> 
                    if (idx == level -1) 
                      then Map.insert (getVPN mode addr level) (pte, level) tlbLevel 
                      else tlbLevel
