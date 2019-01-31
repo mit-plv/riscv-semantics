65c87c7dd4db3b936b59fe3717f158ecd545df00
Thomas Bourgeat <bthom@mit.edu>
2019-01-30 23:00:48 -0500

    Adding a platform that uses in place arrays, goes twice as fast

---
+++ src/Platform/CleanTest.hs
@@ -0,0 +1,171 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables, InstanceSigs, AllowAmbiguousTypes, FlexibleContexts #-}
+module Platform.CleanTest where
+import Spec.Machine 
+import Spec.Decode
+import Utility.Utility
+import Spec.CSRFile
+import qualified Spec.CSRField as Field
+import Data.Bits
+import Data.Int
+import Data.Word
+import Data.Char
+import Data.Maybe
+import Data.IORef
+import Data.Array.IO
+import System.IO.Error
+import qualified Data.Map.Strict as S
+import Control.Monad.State
+import Control.Monad.Trans.Maybe
+import Debug.Trace as T
+
+data VerifMinimal64 = VerifMinimal64 { registers :: IOUArray Register Int64 , 
+                             fpregisters :: IOUArray Register Int32,
+                             csrs :: CSRFile,
+                             privMode :: IORef PrivMode,
+                             pc :: IORef Int64,
+                             nextPC :: IORef Int64,
+                             mem :: IOUArray Int Word8,
+                             reservation :: IORef (Maybe Int)
+                           }
+
+
+type IOState = StateT VerifMinimal64 IO
+
+type LoadFunc = IOState Int32
+type StoreFunc = Int32 -> IOState ()
+
+instance (Show LoadFunc) where
+  show _ = "<loadfunc>"
+instance (Show StoreFunc) where
+  show _ = "<storefunc>"
+
+cGetChar :: IO Int32
+cGetChar = catchIOError (fmap ((fromIntegral:: Int -> Int32). ord) getChar) (\e -> if isEOFError e then return (-1) else ioError e)
+
+rvGetChar = liftIO cGetChar
+rvPutChar val = liftIO (putChar $ chr $ (fromIntegral:: Int32 -> Int) val)
+
+rvZero = return 0
+rvNull val = return ()
+
+getMTime :: LoadFunc
+getMTime = fmap (fromIntegral:: MachineInt -> Int32) (getCSRField Field.MCycle)
+
+-- Ignore writes to mtime.
+setMTime :: StoreFunc
+setMTime _ = return ()
+
+-- Addresses for mtime/mtimecmp chosen for Spike compatibility, and getchar putchar.
+memMapTable :: S.Map MachineInt (LoadFunc, StoreFunc)
+memMapTable = S.fromList [(0x200bff8, (getMTime, setMTime)),(0xfff0, (rvZero, rvPutChar)), (0xfff4, (rvGetChar, rvNull))]
+mtimecmp_addr = 0x2004000 :: Int64
+
+  
+instance RiscvMachine IOState Int64 where
+  getRegister reg = do
+       if reg == 0 
+         then return 0
+         else do 
+           refs <- get
+           lift $ readArray (registers refs) reg
+  setRegister reg val = do
+       if reg == 0
+         then return ()
+         else do 
+           refs <- get
+           lift $ writeArray (registers refs) reg val
+  getFPRegister reg = do
+       refs <- get
+       lift $ readArray (fpregisters refs) reg
+  setFPRegister reg val = do
+       refs <- get
+       lift $ writeArray (fpregisters refs) reg val
+  getPC = do
+       refs <- get
+       lift $ readIORef (pc refs) 
+  setPC npc = do
+       refs <- get
+       lift $ writeIORef (nextPC refs) npc
+  getPrivMode = do
+       refs <- get
+       lift $ readIORef (privMode refs) 
+  setPrivMode val = do
+       refs <- get
+       lift $ writeIORef (privMode refs) val
+  commit = do
+       refs <- get
+       npc <- lift $ readIORef (nextPC refs)
+       lift $ writeIORef (pc refs) npc
+--  -- Wrap Memory instance:
+  loadByte addr = do
+       refs <- get
+       fmap fromIntegral . lift $ readArray (mem refs) (fromIntegral addr) 
+  loadHalf addr = do 
+       refs <- get
+       b0 <- lift . readArray (mem refs) $ fromIntegral addr 
+       b1 <- lift . readArray (mem refs) $ fromIntegral (addr + 1) 
+       return (combineBytes [b0,b1])
+  loadWord :: forall s. (Integral s) => s -> IOState Int32
+  loadWord addr = 
+    case S.lookup ((fromIntegral:: s -> MachineInt) addr) memMapTable of
+      Just (getFunc, _) -> getFunc
+      Nothing -> do
+       refs <- get
+       b0 <- lift . readArray (mem refs) $ fromIntegral addr 
+       b1 <- lift . readArray (mem refs) $ fromIntegral (addr + 1) 
+       b2 <- lift . readArray (mem refs) $ fromIntegral (addr + 2) 
+       b3 <- lift . readArray (mem refs) $ fromIntegral (addr + 3) 
+       return (combineBytes [b0,b1,b2,b3])
+  loadDouble addr = do
+       refs <- get
+       b0 <- lift . readArray (mem refs) $ fromIntegral addr 
+       b1 <- lift . readArray (mem refs) $ fromIntegral (addr + 1) 
+       b2 <- lift . readArray (mem refs) $ fromIntegral (addr + 2) 
+       b3 <- lift . readArray (mem refs) $ fromIntegral (addr + 3) 
+       b4 <- lift . readArray (mem refs) $ fromIntegral (addr + 4) 
+       b5 <- lift . readArray (mem refs) $ fromIntegral (addr + 5) 
+       b6 <- lift . readArray (mem refs) $ fromIntegral (addr + 6) 
+       b7 <- lift . readArray (mem refs) $ fromIntegral (addr + 7) 
+       return (combineBytes [b0,b1,b2,b3,b4,b5,b6,b7])
+  storeByte addr val = do
+       refs <- get
+       lift $ writeArray (mem refs) (fromIntegral addr) (fromIntegral val) -- Convert from Int8 to Word8 
+  storeHalf addr val = do
+       let bytes = splitHalf val
+       refs <- get
+       forM_ (zip bytes [addr + i| i<- [0..]])  $ (\(x,addr)-> lift $ writeArray (mem refs) (fromIntegral addr) (fromIntegral x))
+  storeWord :: forall s. (Integral s, Bits s) => s -> Int32 -> IOState ()
+  storeWord addr val =
+    case S.lookup ((fromIntegral:: s -> MachineInt) addr) memMapTable of
+      Just (_, setFunc) -> setFunc val
+      Nothing -> do 
+       let bytes = splitWord val
+       refs <- get
+       forM_ (zip bytes [addr + i| i<- [0..]])  $ (\(x,addr)-> lift $ writeArray (mem refs) (fromIntegral addr) (fromIntegral x))
+  storeDouble addr val = do
+       let bytes = splitDouble val
+       refs <- get
+       forM_ (zip bytes [addr + i| i<- [0..]])  $ (\(x,addr)-> lift $ writeArray (mem refs) (fromIntegral addr) (fromIntegral x))
+  makeReservation addr = do 
+       refs <- get
+       lift $ writeIORef (reservation refs) (Just $ fromIntegral addr) 
+  checkReservation addr = do
+       refs <- get
+       res <- lift $ readIORef (reservation refs)
+       return (Just (fromIntegral addr) == res)
+  clearReservation addr = do
+       refs <- get
+       lift $ writeIORef (reservation refs) Nothing 
+--  -- CSRs:
+  getCSRField field = do
+     refs <- get
+     return (getField field (csrs refs)) 
+  unsafeSetCSRField :: forall s. (Integral s) => Field.CSRField -> s -> IOState ()
+  unsafeSetCSRField field val = do -- CSRS are not refs in there, because I am lazy.
+     refs <- get
+     put $ refs{csrs = setField field ((fromIntegral:: s -> MachineInt) val) (csrs refs) }
+  inTLB a b = return Nothing -- noTLB
+  addTLB a b c= return ()
+  flushTLB = return ()
+  getPlatform = return (Platform { dirtyHardware = return False, writePlatformCSRField = \field value -> return value })
+
---
+++ src/Platform/RunFast.hs
@@ -0,0 +1,123 @@
+module Platform.RunFast where
+import System.IO
+import System.Environment
+import System.Exit
+import Data.Int
+import Data.IORef
+import Data.Array.IO
+import Data.List
+import Data.Word
+import Utility.Utility
+import Spec.Machine
+import Platform.CleanTest
+import Utility.Elf
+import qualified Spec.CSRField as Field
+import Spec.CSRFile
+import Spec.Decode
+import Spec.Execute
+import Spec.VirtualMemory
+import Spec.Spec
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Writer
+import qualified Data.Map.Strict as S
+import qualified Data.ByteString as B
+import Debug.Trace
+import Numeric (showHex, readHex)
+
+processLine :: String -> (Int, [(Int, Word8)]) -> (Int, [(Int, Word8)])
+processLine ('@':xs) (p, l) = ((fst $ head $ readHex xs) * 4, l)
+processLine s (p, l) = (p + 4, l ++ (zip [p..] $ splitWord (fst $ head $ readHex s :: Word32)))
+
+readHexFile :: FilePath -> IO [(Int, Word8)]
+readHexFile f = do
+  h <- openFile f ReadMode
+  helper h (0, [])
+  where helper h l = do
+          s <- hGetLine h
+          done <- hIsEOF h
+          if (null s)
+            then return $ snd l
+            else if done
+                 then return $ snd $ processLine s l
+                 else helper h (processLine s l)
+
+checkExternalInterrupt :: IO Bool
+checkExternalInterrupt = do
+  ready <- hReady stdin
+  if ready then do
+    c <- hLookAhead stdin
+    if c == '!' then do
+      _ <- getChar
+      _ <- getChar
+      return True
+    else return False
+  else return False
+
+runProgram :: Maybe Int64 -> VerifMinimal64 -> IO (Int64, VerifMinimal64)
+runProgram maybeToHostAddress c =
+  runStateT (stepHelper RV64IMAF maybeToHostAddress (do
+                                                        liftIO checkExternalInterrupt) mtimecmp_addr ) c 
+
+
+
+readProgram :: String -> IO (Maybe Int64, [(Int, Word8)])
+readProgram f = do
+  if ".hex" `isSuffixOf` f
+    then do
+    mem <- readHexFile f
+    return (Nothing, mem)
+    else do
+    mem <- readElf f
+    maybeToHostAddress <- readElfSymbol "tohost" f
+    return (fmap (fromIntegral:: Word64 -> Int64) maybeToHostAddress, mem)
+
+runFile :: String -> IO Int64
+runFile f = do
+  deviceTree <- B.readFile "device_tree.bin"
+  (maybeToHostAddress, program) <- readProgram f
+  -- Create the references and the arrays that are going to be passed around
+  registers <- newArray (0,31) 0   
+  pc <- newIORef 0x80000000
+  fpregisters <- newArray (0,31) 0
+  npc <- newIORef 0
+  privMode <- newIORef Machine
+  mem <- newArray (0,0x00000000ffffffff) 0 -- Create a big 2GB chunk of memory 
+  reservation <- newIORef Nothing
+  putStrLn "All the state is created"
+  -- write the program and the device tree in it
+  let addressCommaByteS = (zip [0..] (B.unpack deviceTree)) ++ program
+  forM_ addressCommaByteS  $ (\(addr,byte)-> writeArray mem (fromIntegral addr) (fromIntegral byte))
+  putStrLn "The program is copied"
+  let c = VerifMinimal64 { registers = registers,
+                      fpregisters = fpregisters,
+                      csrs = (resetCSRFile 64),
+                      pc = pc,
+                      nextPC = npc,
+                      privMode = privMode,
+                      mem = mem,
+                      reservation = reservation } in
+    fmap fst $ runProgram maybeToHostAddress c
+
+runFiles :: [String] -> IO Int64
+runFiles (file:files) = do
+    myreturn <- runFile file
+    putStr (file ++ ": " ++ (show myreturn) ++ "\n")
+    othersreturn <- runFiles files
+    if myreturn /= 0
+        then return myreturn
+        else return othersreturn
+runFiles [] = return 0
+
+main :: IO ()
+main = do
+  args <- getArgs
+  retval <- case args of
+    [] -> do
+      putStr "ERROR: this program expects one or more elf files as command-line arguments\n"
+      return 1
+    [file] -> runFile file
+    files -> runFiles files
+  exitWith (if retval == 0 then ExitSuccess else ExitFailure $ (fromIntegral:: Int64 -> Int) retval)
