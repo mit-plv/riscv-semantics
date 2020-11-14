df4b5811e86070d7724f1214ff0676761dcba554
Samuel Gruetter <samuel.gruetter@epfl.ch>
2020-11-13 21:17:14 -0500

    make clash work again (modulo sed hack)

--- make-circuit.sh
+++ make-circuit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd src
-stack exec -- clash --verilog Platform/Clash.hs
-
-
+sed Spec/Decode.hs -i -e 's/.*if supports. iset then result. else.*/-- REDACTED to make clash work/g' \
+                      -e 's/.*if bitwidth iset == 64.*/-- REDACTED to make clash work/g'
+clash --verilog Platform/Clash.hs
