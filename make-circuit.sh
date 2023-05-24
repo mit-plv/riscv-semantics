cb24c751938c49823c02737dec6028346f59e809
Samuel Gruetter <saemi472@gmail.com>
2023-05-24 01:32:50 -0400

    artifact tweaks

--- make-circuit.sh
+++ make-circuit.sh
@@ -3,3 +3,4 @@ cd src
 sed Spec/Decode.hs -i -e 's/.*if supports. iset then result. else.*/-- REDACTED to make clash work/g' \
                       -e 's/.*if bitwidth iset == 64.*/-- REDACTED to make clash work/g'
 stack exec --package clash-ghc -- clash --verilog Platform/Clash.hs
+git checkout -- Spec/Decode.hs
---
+++ riscv-memory-model/download_alloy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -# -L -O https://alloytools.org/download/alloy4.2_2015-02-22.jar
